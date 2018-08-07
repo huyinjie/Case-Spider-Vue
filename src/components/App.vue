a6e46340b359e53ef889bfd7c7bc5b501d0c80ee
huyinjie <huyinjie1998@gmail.com>
2018-08-07 15:59:25 +0800

    Jonsbo Test

--- src/components/App.vue
+++ src/components/App.vue
@@ -49,7 +49,28 @@ export default {
         },
       ],
       rows: [
-        
+        {
+        "Product name": "U1 PLUS",
+        "Support Color": "银色,黑色",
+        "Width": 170,
+        "Depth": 220,
+        "Height": 302,
+        "Material": "Main body: 4.0MM thick aluminum magnesium alloy; side plate: 5MM toughened glass.",
+        "Motherboard Support": "  MINI-ITX",
+        "Drive Bays": "2*2.5〞",
+        "Front I/O": "USB3.0*2 ,  Headphone*1 , Mic*1"
+    },
+    {
+        "Product name": "U4",
+        "Support Color": "银色,黑色,蓝色,金色,粉色,红色,白色",
+        "Width": 205,
+        "Depth": 340,
+        "Height": 428,
+        "Material": "The main body of U is 2.0MM Al Mg alloy, and the right plate / rear window / side hard disk is made of 1.5MM aluminum magnesium alloy, and the left plate is fully toughened glass with 5.0MM, and the thickness of the internal metal plate is 1.0MMSGCC steel plate.",
+        "Motherboard Support": "  ITX / M-ATX / ATX （305*245MM Maximum）  ",
+        "Drive Bays": "1*3.5 \"+ 3*2.5\" or 2*3.5 \"+ 2*2.5\"",
+        "Front I/O": "USB3.0*2 ,   Headphone*1 , Mic*1"
+    },
       ],
     };
   },
